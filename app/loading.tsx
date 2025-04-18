ec0ef104c4acfd6ba9c27774604a25a64388096c
seo-dongchan <dongchan.seo@searchm.co.kr>
2025-04-18 16:15:25 +0900

    add : 페이지 이동 시 로딩 처리

---
+++ app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-white/80">
+      <div className="h-8 w-8 border-4 border-primary border-t-transparent rounded-full" />
+    </div>
+  );
+}
