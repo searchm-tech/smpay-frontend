3ec3ac21883ad4381e7eb0c3d4039438e2853ad4
seo-dongchan <dongchan.seo@searchm.co.kr>
2025-04-21 13:55:57 +0900

    add : 충전 현황 페이지 개발 + 기능

---
+++ components/views/sm-pay/charge/index.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { useRoleStore } from "@/store/useRoleStore";
+import SMPayChargeAdminView from "./admin";
+import SMPayChargeAgencyView from "./agency";
+
+// TODO : 관리자, 대행사 권한에 따라 화면 불리...
+// 페이지를 따로 분리를 해야하는지 고민 필요
+
+const SMPayChargeView = () => {
+  const { role } = useRoleStore();
+
+  return (
+    <div>
+      {role === "admin" && <SMPayChargeAdminView />}
+      {role === "agency" && <SMPayChargeAgencyView />}
+    </div>
+  );
+};
+
+export default SMPayChargeView;
