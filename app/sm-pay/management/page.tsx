d0b93e79384c60ec7084391f85d946bcc3baeccb
seo-dongchan <dongchan.seo@searchm.co.kr>
2025-04-21 14:20:21 +0900

    fix : 로딩 관련 테스트 중

--- app/sm-pay/management/page.tsx
+++ app/sm-pay/management/page.tsx
@@ -1,12 +1,19 @@
+import { Suspense } from "react";
 import SMPayManagementView from "@/components/views/sm-pay/manangement";
 import ContentHeader from "@/components/common/ContentHeader";
+import LoadingUI from "@/components/common/Loading";
+
 import type { DashboardSubItem } from "@/constants/dasboard";
 
 export default function SMPayManagementPage() {
   return (
     <div>
       <ContentHeader title="SM Pay 관리" items={breadcrumbItems} />
-      <SMPayManagementView />
+      <Suspense
+        fallback={<LoadingUI title="SM Pay 관리 페이지2222 로딩중..." />}
+      >
+        <SMPayManagementView />
+      </Suspense>
     </div>
   );
 }
