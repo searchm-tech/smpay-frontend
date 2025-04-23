ed058bed665bbdd413444c42822db593677979b3
seo-dongchan <dongchan.seo@searchm.co.kr>
2025-04-23 15:22:41 +0900

    add : Title 모바일 용 추가

--- components/common/Title.tsx
+++ components/common/Title.tsx
@@ -12,3 +12,16 @@ const Title = () => {
 };
 
 export default Title;
+
+export const MobileTitle = () => {
+  return (
+    <div className="w-full shadow-[0_2px_4px_-2px_rgba(0,0,0,0.2)]">
+      <div className="max-w-[400px] h-[70px] flex items-center justify-center">
+        <div className="flex items-center justify-center text-[22px] font-bold">
+          <span className="text-[#FF7700]">SM Pay</span>
+          <span className="ml-1">- 온라인 광고 결제 솔루션</span>
+        </div>
+      </div>
+    </div>
+  );
+};
