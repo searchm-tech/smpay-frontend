86279f214637c22ba8b15278702087002652ac24
seo-dongchan <dongchan.seo@searchm.co.kr>
2025-04-25 12:03:48 +0900

    SPP-31 refact : 테이블 관련 이슈 수정

--- types/table.ts
+++ types/table.ts
@@ -1,12 +1,13 @@
-import type { SortOrder, FilterValue } from "antd/es/table/interface";
+import type { TablePaginationConfig } from "antd/es/table";
+import type { FilterValue, SortOrder } from "antd/es/table/interface";
 
 export interface TableParams {
-  pagination?: {
+  pagination: {
     current: number;
     pageSize: number;
     total?: number;
   };
   sortField?: string;
   sortOrder?: SortOrder;
-  filters?: Record<string, FilterValue | null>;
+  filters?: Record<string, FilterValue>;
 }
