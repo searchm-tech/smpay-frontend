92bbcbad4e7d00c1e2438f58f40a074f60b58d7d
seo-dongchan <dongchan.seo@searchm.co.kr>
2025-04-28 15:55:43 +0900

    SPP-46 feat : E0240 모바일 > 개인정보 수집  기능 및 추가 ui 개발

--- components/views/advertiser-verification/mobile/AccountCharge.tsx
+++ components/views/advertiser-verification/mobile/AccountCharge.tsx
@@ -1,8 +1,10 @@
 import { Button } from "@/components/ui/button";
-
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import Select from "@/components/composite/select-components";
+import { NumberInput } from "@/components/composite/input-components";
+
+import type { AccountInfo } from "..";
 
 const bankOptions = [
   { label: "농협", value: "088" },
@@ -11,8 +13,17 @@ const bankOptions = [
   { label: "국민", value: "083" },
 ];
 
-const AccountCharge = () => {
-  // TODO 점선 컴포넌트 적용
+type AccountChargeProps = {
+  chargeAccount: AccountInfo;
+  setChargeAccount: (value: AccountInfo) => void;
+  handleReset: () => void;
+};
+
+const AccountCharge = ({
+  chargeAccount,
+  setChargeAccount,
+  handleReset,
+}: AccountChargeProps) => {
   return (
     <section className="mt-8 w-full px-4">
       <div className="flex flex-col gap-4">
@@ -23,26 +34,54 @@ const AccountCharge = () => {
           <Select
             options={bankOptions}
             placeholder="충전 계좌 은행을 선택해주세요."
+            value={chargeAccount.bank}
+            onChange={(value) =>
+              setChargeAccount({ ...chargeAccount, bank: value })
+            }
           />
         </div>
 
         <div className="flex flex-col gap-2">
           <Label className="text-sm font-medium">충전 계좌 은행 *</Label>
-          <Input className="max-w-[500px] h-[35px]" />
+          <NumberInput
+            className="max-w-[500px]"
+            value={chargeAccount.accountNumber}
+            onChange={(value) =>
+              setChargeAccount({ ...chargeAccount, accountNumber: value })
+            }
+          />
         </div>
 
         <div className="flex flex-col gap-2 mb-8">
-          <Label className="text-sm font-medium">충전 계좌 은행 *</Label>
-          <Input className="max-w-[500px] h-[35px]" />
+          <Label className="text-sm font-medium">충전 계좌 예금주명 *</Label>
+          <Input
+            className="max-w-[500px]"
+            value={chargeAccount.accountHolder}
+            onChange={(e) =>
+              setChargeAccount({
+                ...chargeAccount,
+                accountHolder: e.target.value,
+              })
+            }
+          />
         </div>
 
         <div className="flex justify-center gap-2">
-          <Button className="h-[35px] w-1/2">
-            <span className="font-bold">계좌 인증하기</span>
+          <Button
+            className="h-[35px] w-1/2 font-bold"
+            onClick={() => {
+              alert("계좌 인증이 완료 되었습니다.");
+            }}
+          >
+            계좌 인증하기
           </Button>
 
-          <Button className="h-[35px] w-1/2" variant="cancel">
-            <span className="font-bold">초기화</span>
+          <Button
+            className="h-[35px] w-1/2 font-bold"
+            variant="cancel"
+            onClick={handleReset}
+          >
+            초기화
           </Button>
         </div>
       </div>
--- components/views/advertiser-verification/mobile/AgreemenSection.tsx
+++ components/views/advertiser-verification/mobile/AgreemenSection.tsx
@@ -3,21 +3,65 @@ import { Separator } from "@/components/ui/separator";
 import { Switch } from "@/components/ui/switch";
 import { Label } from "@/components/ui/label";
 
-const AgreemenSection = () => {
+type AgreemenSectionProps = {
+  agreed: boolean;
+  setAgreed: (value: boolean) => void;
+  agreePrivacy: boolean;
+  setAgreePrivacy: (value: boolean) => void;
+  agreeService: boolean;
+  setAgreeService: (value: boolean) => void;
+};
+
+const AgreemenSection = ({
+  agreed,
+  setAgreed,
+  agreePrivacy,
+  setAgreePrivacy,
+  agreeService,
+  setAgreeService,
+}: AgreemenSectionProps) => {
+  const handleAllAgree = () => {
+    if (agreed) {
+      setAgreed(false);
+      setAgreePrivacy(false);
+      setAgreeService(false);
+    } else {
+      setAgreed(true);
+      setAgreePrivacy(true);
+      setAgreeService(true);
+    }
+  };
+
+  const handleAgreePrivacy = () => {
+    if (agreed && agreePrivacy) {
+      setAgreed(false);
+    }
+    setAgreePrivacy(!agreePrivacy);
+  };
+
+  const handleAgreeService = () => {
+    if (agreed && agreeService) {
+      setAgreed(false);
+    }
+    setAgreeService(!agreeService);
+  };
+
   return (
     <section className="mt-8 w-full px-4">
       <div className="flex items-center space-x-2">
-        <Switch id="airplane-mode" size="sm" />
-        <Label htmlFor="airplane-mode" className="font-medium">
-          Airplane Mode
-        </Label>
+        <Switch size="sm" checked={agreed} onCheckedChange={handleAllAgree} />
+        <Label className="font-medium">전체 동의</Label>
       </div>
 
       <Separator className="mt-4 border-black border-[1px]" />
 
       <div className="flex flex-col gap-2">
         <div className="flex items-center gap-2 pb-3 pt-5">
-          <Switch id="airplane-mode" size="sm" />
+          <Switch
+            size="sm"
+            checked={agreePrivacy}
+            onCheckedChange={handleAgreePrivacy}
+          />
           <span className="text-[14px] font-medium">
             [필수] 개인 정보 수집 이용에 동의합니다.
           </span>
@@ -26,7 +70,11 @@ const AgreemenSection = () => {
         <div className="border-dotted border-gray-400 border-b-2 border-t-1" />
 
         <div className="flex items-center gap-2 py-3">
-          <Switch id="airplane-mode" size="sm" />
+          <Switch
+            size="sm"
+            checked={agreeService}
+            onCheckedChange={handleAgreeService}
+          />
           <span className="text-[14px] font-medium">
             [필수] SM Pay 부가 서비스 이용에 동의합니다.
           </span>
