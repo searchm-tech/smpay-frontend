36f185ab660fd5fb1dcc3de100c767d5f8868cf2
seo-dongchan <dongchan.seo@searchm.co.kr>
2025-05-08 15:53:08 +0900

    SPP-39 feat : select-search-sever 기반으로 대행사 select박스 추가

--- components/views/account/membership-register/DirectRegistSection.tsx
+++ components/views/account/membership-register/DirectRegistSection.tsx
@@ -9,24 +9,28 @@ import {
   DescriptionItem,
   Descriptions,
 } from "@/components/composite/description-components";
+import { SelectSearchServer } from "@/components/composite/select-search-server";
 
-import Select from "@/components/composite/select-components";
 import { RadioGroup } from "@/components/composite/radio-component";
+
+import { fetchAdvertisers } from "@/services/advertiser";
+import type { TableParams } from "@/services/types";
 
 const optsRadio = [
   { label: "그룹장", value: "leader" },
   { label: "그룹원", value: "member" },
 ];
 
-const optsSelect = [
-  { label: "일별조회", value: "daily" },
-  { label: "주별조회", value: "weekly" },
-  { label: "월별조회", value: "monthly" },
+const optionAgency = [
+  { label: "주식회사 써치엠 | 홍길동", value: "1" },
+  { label: "주식회사 써치엠 | 김철수", value: "2" },
+  { label: "주식회사 써치엠 | 이영희", value: "3" },
 ];
 
 const DirectRegistSection = () => {
   const [phone, setPhone] = useState("");
   const [selected, setSelected] = useState("leader");
+  const [selectedValue, setSelectedValue] = useState("");
 
   return (
     <div className="py-4">
@@ -34,13 +38,14 @@ const DirectRegistSection = () => {
         회원 정보
       </LabelBullet>
       <Descriptions columns={1} bordered>
-        <DescriptionItem label="부서 선택 *">
-          <Select
-            options={optsSelect}
-            value={selected}
-            onChange={setSelected}
-            placeholder="구분 : 일별조회"
-            className="w-[500px]"
+        <DescriptionItem label="대행사 선택 *">
+          <SelectSearchServer
+            className="max-w-[500px]"
+            fetchOptions={fetchAdvertiserOptions}
+            value={selectedValue}
+            onValueChange={setSelectedValue}
+            placeholder="광고주를 선택하세요"
+            searchPlaceholder="광고주명 또는 ID 검색..."
           />
         </DescriptionItem>
         <DescriptionItem label="회원 구분 *">
@@ -91,3 +96,22 @@ const DirectRegistSection = () => {
 };
 
 export default DirectRegistSection;
+
+/**
+ * 무한스크롤에 맞는 형태의 함수  (기존 api 함수를 변형)
+ * @param params
+ * @returns hasNextPage 다음 페이지 존재 여부
+ */
+async function fetchAdvertiserOptions(params: TableParams) {
+  const response = await fetchAdvertisers(params);
+
+  return {
+    items: response.data.map((advertiser) => ({
+      label: `${advertiser.advertiserName} | ${advertiser.name}`,
+      value: advertiser.customerId,
+    })),
+    hasNextPage:
+      response.total >
+      (params.pagination?.current || 1) * (params.pagination?.pageSize || 10),
+  };
+}
