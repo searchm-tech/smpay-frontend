48b7538230e0c1df925af23fd91d09c6d518aea5
seo-dongchan <dongchan.seo@searchm.co.kr>
2025-05-12 11:30:40 +0900

    feat : 메일 만료, 로그인 만료, 계정비활성화, 계정 삭제 페이지

--- app/expiration-mail/page.tsx
+++ app/expiration-mail/page.tsx
@@ -1,20 +1,22 @@
-// app/not-found/page.tsx
-"use client";
-
 import { HomeButton } from "@/components/composite/button-components";
 import Image from "next/image";
 
-// 500 오류 페이지
-export default function ErrorPage() {
+// 404 오류 페이지
+export default function ExpirationMailPage() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen text-center p-4">
       <div className="my-5 w-40 h-40 md:w-48 md:h-48 lg:w-56 lg:h-56 rounded-full bg-gray-100 flex items-center justify-center">
         <Image src="/images/error.png" alt="logo" width={100} height={100} />
       </div>
       <p className="text-[20px] font-medium">
-        500 오류 - 서버 오류가 발생했습니다.
+        인증 메일의 유효기간이 만료되었습니다.
       </p>
-      <p className="my-8 text-[15px] font-medium">잠시 후 다시 시도해주세요.</p>
+      <p className="my-8 text-[15px] font-medium">
+        인증 메일은 발송 후 7일(3일) 동안 유효합니다.
+        <br />
+        <br />
+        새로운 인증 메일이 필요하시면 담당자에게 재발송을 요청해주세요.
+      </p>
       <HomeButton href="/">메인 페이지로 이동</HomeButton>
     </div>
   );
