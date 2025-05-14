4169cf71c08addba990059e17fcc77c9574303d7
seo-dongchan <dongchan.seo@searchm.co.kr>
2025-05-14 12:05:18 +0900

    refact : SM Pay 신청 화면 관련 form 형태 기반 수정으로 인한 리펙토리

--- components/views/sm-pay/components/AdvertiserDesc.tsx
+++ components/views/sm-pay/components/AdvertiserDesc.tsx
@@ -2,35 +2,49 @@ import {
   Descriptions,
   DescriptionItem,
 } from "@/components/composite/description-components";
+import { LabelBullet } from "@/components/composite/label-bullet";
+import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import type { AdvertiserData } from "@/types/adveriser";
 
 type AdvertiserDescProps = {
   advertiserDetail: AdvertiserData | null;
+  onEdit: () => void;
 };
 
-const AdvertiserDesc = ({ advertiserDetail }: AdvertiserDescProps) => {
+const AdvertiserDesc = ({ advertiserDetail, onEdit }: AdvertiserDescProps) => {
   return (
-    <Descriptions columns={1}>
-      <DescriptionItem label="사업자명">
-        <Label>{advertiserDetail?.businessName}</Label>
-      </DescriptionItem>
-      <DescriptionItem label="광고주 닉네임">
-        <Label>{advertiserDetail?.loginId}</Label>
-      </DescriptionItem>
-      <DescriptionItem label="대표자명">
-        <Label>{advertiserDetail?.businessOwnerName}</Label>
-      </DescriptionItem>
-      <DescriptionItem label="사업자 등록 번호">
-        <Label>{advertiserDetail?.businessNumber}</Label>
-      </DescriptionItem>
-      <DescriptionItem label="담당자 휴대폰 번호">
-        <Label>{advertiserDetail?.businessOwnerPhone}</Label>
-      </DescriptionItem>
-      <DescriptionItem label="담당자 이메일 주소">
-        <Label>{advertiserDetail?.businessOwnerEmail}</Label>
-      </DescriptionItem>
-    </Descriptions>
+    <section>
+      <div className="flex items-center gap-4 pb-4">
+        <LabelBullet labelClassName="text-base font-bold">
+          광고주 기본 정보
+        </LabelBullet>
+
+        <Button className="w-[100px]" onClick={onEdit}>
+          변경하기
+        </Button>
+      </div>
+      <Descriptions columns={1}>
+        <DescriptionItem label="사업자명">
+          <Label>{advertiserDetail?.businessName}</Label>
+        </DescriptionItem>
+        <DescriptionItem label="광고주 닉네임">
+          <Label>{advertiserDetail?.loginId}</Label>
+        </DescriptionItem>
+        <DescriptionItem label="대표자명">
+          <Label>{advertiserDetail?.businessOwnerName}</Label>
+        </DescriptionItem>
+        <DescriptionItem label="사업자 등록 번호">
+          <Label>{advertiserDetail?.businessNumber}</Label>
+        </DescriptionItem>
+        <DescriptionItem label="담당자 휴대폰 번호">
+          <Label>{advertiserDetail?.businessOwnerPhone}</Label>
+        </DescriptionItem>
+        <DescriptionItem label="담당자 이메일 주소">
+          <Label>{advertiserDetail?.businessOwnerEmail}</Label>
+        </DescriptionItem>
+      </Descriptions>
+    </section>
   );
 };
 
