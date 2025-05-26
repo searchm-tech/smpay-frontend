9a866615ca976f1dbba8a22c7c29b09684937e6f
seo-dongchan <dongchan.seo@searchm.co.kr>
2025-05-26 11:45:56 +0900

    SPP-82 chore : 불필요한 코드 정리

--- components/views/sign-up/ModalSuccess.tsx
+++ components/views/sign-up/ModalSuccess.tsx
@@ -15,16 +15,9 @@ const ModalSuccess = ({ onClose }: ModalSuccessProps) => {
       iconDisabled
     >
       <div className="w-[800px]">
-        <div className="text-center text-base font-bold pb-4 border-b border-[#C0C0C0] border-dotted">
+        <div className="text-center text-base font-bold">
           <p>회원가입이 완료되었습니다. 메일로 로그인 링크가 발송되었습니다.</p>
           <p className="mb-4">로그인 후 SM Pay 서비스 이용이 가능합니다.</p>
-        </div>
-
-        <div className="border-b border-[#C0C0C0] py-5 font-medium text-base pl-32">
-          <span>아이디</span>
-          <span className="text-[#A1A1A1]">
-            (가입시 입력한 이메일 주소의 아이디 부분)
-          </span>
         </div>
       </div>
     </Modal>
