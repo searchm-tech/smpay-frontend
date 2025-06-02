bf6ff5ad53f414f6cf2dd6a50e27de0ea0f8e73b
seo-dongchan <dongchan.seo@searchm.co.kr>
2025-06-02 16:00:29 +0900

    SPP-103 feat : API 라이선스 수정, 삭제 관련 ui 추가 개발 (api 미동작)

--- components/views/naver-service/index.tsx
+++ components/views/naver-service/index.tsx
@@ -1,25 +1,81 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
 
 import AdvertiserSection from "./AdvertiserSection";
 import LicenseSection from "./LicenseSection";
+import { NoLicenseDialog } from "./dialog";
+
 import { LabelBullet } from "@/components/composite/label-bullet";
+
 import { TabSwitch } from "@/components/composite/tab-switch";
 
+import { getAgentsUserLicense } from "@/services/license";
+
+import type { TResponseLicense } from "@/types/api/license";
+
+export type TLicenseInfo = {
+  userId: number;
+  agentId: number;
+  customerId: number;
+  apiKey: string;
+  secretKey: string;
+};
+
 const NaverServiceView = () => {
+  const { data: session } = useSession();
+
   const [isCustomerStep, setIsCustomerStep] = useState(false);
+  const [licenseInfo, setLicenseInfo] = useState<TLicenseInfo | null>({
+    userId: 1,
+    agentId: 1,
+    customerId: 0,
+    apiKey: "",
+    secretKey: "",
+  });
+  const [isNoLicenseDialogOpen, setIsNoLicenseDialogOpen] = useState(false);
+
+  const handleChangeStep = (value: boolean) => {
+    if (value) {
+      // TODO : api 정상 동작하면 그때 적용
+      // if (!licenseInfo) {
+      //   setIsNoLicenseDialogOpen(true);
+      //   return;
+      // } else {
+      //   setIsCustomerStep(true);
+      // }
+      setIsCustomerStep(true);
+    } else {
+      setIsCustomerStep(false);
+    }
+  };
+
+  useEffect(() => {
+    // TODO : api 정상 동작하면 그때 적용
+    // if (session?.user) {
+    //   const { agentId, userId } = session.user;
+    //   getAgentsUserLicense(agentId.toString(), userId.toString()).then(
+    //     (res) => {
+    //       setLicenseInfo(res);
+    //     }
+    //   );
+    // }
+  }, [session?.user]);
 
   return (
     <div>
+      {isNoLicenseDialogOpen && (
+        <NoLicenseDialog onClose={() => setIsNoLicenseDialogOpen(false)} />
+      )}
       <LabelBullet labelClassName="text-base font-bold">정보 등록</LabelBullet>
       <TabSwitch
         value={isCustomerStep}
-        onValueChange={setIsCustomerStep}
+        onValueChange={handleChangeStep}
         leftLabel="API 라이선스 등록"
         rightLabel="광고주 등록"
       />
-      {!isCustomerStep && <LicenseSection />}
+      {!isCustomerStep && <LicenseSection licenseInfo={licenseInfo} />}
       {isCustomerStep && <AdvertiserSection />}
     </div>
   );
