3ec04d7b54bfdab3247af5bd13bc6669469c2b11
seo-dongchan <dongchan.seo@searchm.co.kr>
2025-06-16 17:34:55 +0900

    SPP-126 refact : SM Pay 관리 > 모달 관련 1차 리펙토링

--- components/views/sm-pay/judgement/TableSection.tsx
+++ components/views/sm-pay/judgement/TableSection.tsx
@@ -13,7 +13,7 @@ import type { ColumnsType } from "antd/es/table";
 import type { SmPayJudgementData } from "@/types/sm-pay";
 import type { TableProps } from "antd";
 
-import StopInfoModal from "../components/StopInfoModal";
+import { StopInfoModal } from "../manangement/dialog";
 
 type PropsTableSection = {
   dataSource: SmPayJudgementData[];
