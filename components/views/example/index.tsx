6ee56f6530344a4e5507520e25a1e0c2f5f655bf
seo-dongchan <dongchan.seo@searchm.co.kr>
2025-06-16 14:52:47 +0900

    SPP-126 refact : 레이아웃 수정

--- components/views/example/index.tsx
+++ components/views/example/index.tsx
@@ -1,16 +1,22 @@
+"use client";
+import { useState } from "react";
+
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import ColorShowcase from "./ColorShowcase";
 import ColorTest from "./ColorTest";
 import UIShowCase from "./UIShowCase";
+import NavShowCase from "./NavShowCase";
 
 const ExampleView = () => {
+  const [value, setValue] = useState("1");
   return (
     <div>
-      <Tabs defaultValue="1">
+      <Tabs value={value} onValueChange={setValue}>
         <TabsList>
           <TabsTrigger value="1">UIShowCase</TabsTrigger>
           <TabsTrigger value="2">ColorShowcase</TabsTrigger>
           <TabsTrigger value="3">ColorTest</TabsTrigger>
+          <TabsTrigger value="4">NavShowCase</TabsTrigger>
         </TabsList>
         <TabsContent value="1">
           <UIShowCase />
@@ -20,6 +26,11 @@ const ExampleView = () => {
         </TabsContent>
         <TabsContent value="3">
           <ColorTest />
+        </TabsContent>
+        <TabsContent value="4">
+          <div className="h-[100vh] mb-10">
+            <NavShowCase onClose={() => setValue("1")} />
+          </div>
         </TabsContent>
       </Tabs>
     </div>
